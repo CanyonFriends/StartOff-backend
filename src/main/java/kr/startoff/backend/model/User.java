deb6f80e5088064b72b7b5b635231d2000d06fa2
SeoSeunghun <proto_seo@naver.com>
2021-08-01 18:47:53 +0900

    [feat] [#1] User 클래스 생성
    UserDetails 를 상속받는 User 클래스 생성

---
+++ src/main/java/kr/startoff/backend/model/User.java
@@ -0,0 +1,75 @@
+package kr.startoff.backend.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    Long id;
+
+    @Column(name = "password",nullable = false)
+    String password;
+
+    @Column(name = "email",nullable = false,unique = true)
+    String email;
+
+    @Column(name = "nickname",nullable = false,unique = true)
+    String nickname;
+
+    @Column(name = "github_url")
+    String githubUrl;
+
+    @Column(name = "blog_url")
+    String blogUrl;
+
+    @Column(name = "baekjoon_id")
+    String baekjoonId;
+
+    @Column(name = "introduce")
+    String introduce;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
