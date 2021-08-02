f1ffc6770b75c03aa30fb0a4863446678d36f550
SeoSeunghun <proto_seo@naver.com>
2021-08-02 14:31:31 +0900

    [feat][#1] 빠진 애노테이션 추가

--- src/main/java/kr/startoff/backend/service/UserDetailsServiceImpl.java
+++ src/main/java/kr/startoff/backend/service/UserDetailsServiceImpl.java
@@ -1,12 +1,12 @@
 package kr.startoff.backend.service;
 
 import kr.startoff.backend.exception.UserNotFoundException;
-import kr.startoff.backend.model.User;
 import kr.startoff.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +14,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) {
         return userRepository.findByEmail(username)
                 .orElseThrow(UserNotFoundException::new);
