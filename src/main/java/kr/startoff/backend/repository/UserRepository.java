9fe930839aa4673f5a5c274da7eda81348a90024
SeoSeunghun <proto_seo@naver.com>
2021-08-02 14:36:40 +0900

    [refactor][#1] User 저장하는 패키지 이름 변경

--- src/main/java/kr/startoff/backend/repository/UserRepository.java
+++ src/main/java/kr/startoff/backend/repository/UserRepository.java
@@ -1,8 +1,7 @@
 package kr.startoff.backend.repository;
 
-import kr.startoff.backend.model.User;
+import kr.startoff.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
