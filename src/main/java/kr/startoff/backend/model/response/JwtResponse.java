12d7d7365b3d9dccac40d40b7c29ef57f9209ca5
SeoSeunghun <proto_seo@naver.com>
2021-08-04 10:46:23 +0900

    [feat][#2] 로그인 시 Response Body에 token을 담아줄 객체 생성

---
+++ src/main/java/kr/startoff/backend/model/response/JwtResponse.java
@@ -0,0 +1,21 @@
+package kr.startoff.backend.model.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class JwtResponse {
+    private String accessToken;
+    private String refreshToken;
+    private Long userId;
+    private String email;
+
+    public JwtResponse(String accessToken, String refreshToken, Long userId, String email) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.userId = userId;
+        this.email = email;
+    }
+}
