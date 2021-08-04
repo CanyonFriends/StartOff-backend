330558f7e2865bb69d8b027f2defb91207a711cc
SeoSeunghun <proto_seo@naver.com>
2021-08-04 10:32:46 +0900

    [feat][#2] RefreshToken 관련 예외 생성

---
+++ src/main/java/kr/startoff/backend/exception/TokenRefreshException.java
@@ -0,0 +1,10 @@
+package kr.startoff.backend.exception;
+
+public class TokenRefreshException extends RuntimeException{
+    public TokenRefreshException() {
+    }
+
+    public TokenRefreshException(String message) {
+        super(message);
+    }
+}
