7efc483d706a5538e00c0343c3e568da64f75147
SeoSeunghun <proto_seo@naver.com>
2021-08-06 17:36:05 +0900

    [feat][#4] User 관련 API 추가
    - 유저 개인정보 받아오는 API
    - 유저 정보 수정하는 API
    - 회원탈퇴 API
    - 유저 프로필 화면 정보 구성을 위한 API

--- src/main/java/kr/startoff/backend/controller/UserController.java
+++ src/main/java/kr/startoff/backend/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.startoff.backend.model.request.UserInfoUpdateRequest;
 import kr.startoff.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 
@@ -36,6 +38,29 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 		}
 		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+
+	// TODO : Design 이 결정 되면 확실하게 로직 설계
+	@GetMapping("/users/{user_id}")
+	public ResponseEntity<?> getUserInformation(@PathVariable(value = "user_id") Long userId) {
+		return ResponseEntity.ok(userService.getUserInformation(userId));
+	}
+
+	// TODO : Design 이 결정 되면 확실하게 로직 설계
+	@PutMapping("/users/{user_id}")
+	public ResponseEntity<?> updateUserInformation(@PathVariable(value = "user_id") Long userId,
+		@RequestBody UserInfoUpdateRequest updateRequest) {
+		return ResponseEntity.ok(userService.updateUser(updateRequest, userId));
+	}
+
+	@DeleteMapping("/users/{user_id}")
+	public ResponseEntity<?> leaveMembership(@PathVariable(value = "user_id") Long userId) {
+		return ResponseEntity.ok(userService.deleteUser(userId));
+	}
+
+	@GetMapping("/users/profile/{user_id}")
+	public ResponseEntity<?> getUserProfile(@PathVariable(value = "user_id") Long userId) {
+		return ResponseEntity.ok(userService.getUserProfile(userId));
 	}
 
 }
