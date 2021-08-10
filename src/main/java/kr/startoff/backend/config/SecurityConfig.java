f039b5bd0f02ee3c28684c762fbe6bbd44b5abbc
SeoSeunghun <proto_seo@naver.com>
2021-08-10 16:16:52 +0900

    [feat][#10] github login을 위한 security config 수정

--- src/main/java/kr/startoff/backend/config/SecurityConfig.java
+++ src/main/java/kr/startoff/backend/config/SecurityConfig.java
@@ -16,7 +16,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import kr.startoff.backend.security.jwt.AuthTokenFilter;
 import kr.startoff.backend.security.jwt.JwtAccessDeniedHandler;
 import kr.startoff.backend.security.jwt.JwtAuthEntryPoint;
-import kr.startoff.backend.security.jwt.JwtUtils;
+import kr.startoff.backend.security.jwt.JwtUtil;
+import kr.startoff.backend.security.oauth2.CustomOAuth2UserService;
+import kr.startoff.backend.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import kr.startoff.backend.security.oauth2.OAuth2AuthenticationFailureHandler;
+import kr.startoff.backend.security.oauth2.OAuth2AuthenticationSuccessHandler;
 import kr.startoff.backend.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 
@@ -26,13 +30,27 @@ import lombok.RequiredArgsConstructor;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private final UserDetailsServiceImpl userDetailsService;
-	private final JwtUtils jwtUtils;
+	private final JwtUtil jwtUtil;
 	private final JwtAuthEntryPoint unauthorizedHandler;
 	private final JwtAccessDeniedHandler accessDeniedHandler;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+	private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+
+	/*
+	  By default, Spring OAuth2 uses HttpSessionOAuth2AuthorizationRequestRepository to save
+	  the authorization request. But, since our service is stateless, we can't save it in
+	  the session. We'll save the request in a Base64 encoded cookie instead.
+	*/
+	@Bean
+	public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
+		return new HttpCookieOAuth2AuthorizationRequestRepository();
+	}
 
 	@Bean
 	public AuthTokenFilter authTokenFilter() {
-		return new AuthTokenFilter(jwtUtils, userDetailsService);
+		return new AuthTokenFilter(jwtUtil, userDetailsService);
 	}
 
 	@Override
@@ -54,6 +72,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
+			.formLogin()
+			.disable()
+			.httpBasic()
+			.disable()
 			.exceptionHandling()
 			.authenticationEntryPoint(unauthorizedHandler)
 			.accessDeniedHandler(accessDeniedHandler)
@@ -61,8 +83,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeRequests()
-			.antMatchers("/api/**").permitAll()
-			.anyRequest().authenticated();
+			.antMatchers("/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.oauth2Login()
+			.authorizationEndpoint()
+			.baseUri("/oauth2/authorize")
+			.authorizationRequestRepository(cookieAuthorizationRequestRepository())
+			.and()
+			.userInfoEndpoint()
+			.userService(customOAuth2UserService)
+			.and()
+			.successHandler(oAuth2AuthenticationSuccessHandler)
+			.failureHandler(oAuth2AuthenticationFailureHandler);
 
 		http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
