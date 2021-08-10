c59b7e5d94adfe33cf67eda22ffb93b111af4604
SeoSeunghun <proto_seo@naver.com>
2021-08-10 16:16:31 +0900

    [feat][#10] JwtResponse에 header 정보 추가

--- src/main/java/kr/startoff/backend/model/response/JwtResponse.java
+++ src/main/java/kr/startoff/backend/model/response/JwtResponse.java
@@ -1,5 +1,6 @@
 package kr.startoff.backend.model.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -8,11 +9,12 @@ import lombok.Getter;
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class JwtResponse {
+	@JsonProperty("token_type")
+	private final String TOKEN_TYPE = "Bearer";
 	private String accessToken;
 	private String refreshToken;
 	private Long userId;
 	private String email;
-
 	public JwtResponse(String accessToken, String refreshToken, Long userId, String email) {
 		this.accessToken = accessToken;
 		this.refreshToken = refreshToken;
