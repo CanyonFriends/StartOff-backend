92fa027c79be49315e38a4942dedf7db8661f05d
SeoSeunghun <proto_seo@naver.com>
2021-08-12 17:15:20 +0900

    [refactor][#13] Exception을 throw 하도록 수정

--- src/main/java/kr/startoff/backend/controller/AuthController.java
+++ src/main/java/kr/startoff/backend/controller/AuthController.java
@@ -3,6 +3,7 @@ package kr.startoff.backend.controller;
 import java.net.URI;
 import java.util.Optional;
 
+import javax.security.auth.login.FailedLoginException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,9 +12,11 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import kr.startoff.backend.entity.User;
+import kr.startoff.backend.exception.EmailOrNicknameDuplicateException;
+import kr.startoff.backend.exception.InvalidPasswordException;
 import kr.startoff.backend.exception.TokenRefreshException;
 import kr.startoff.backend.model.request.LoginRequest;
 import kr.startoff.backend.model.request.SignupRequest;
@@ -42,10 +47,15 @@ public class AuthController {
 	private final UserDetailsServiceImpl userDetailsService;
 	private final JwtUtil jwtUtil;
 	private final RedisUtil redisUtil;
-	private final CookieUtil cookieUtil;
 
 	@PostMapping("/signup")
 	public ResponseEntity<?> signUp(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userService.isDuplicateEmail(signUpRequest.getEmail())) {
+			throw new EmailOrNicknameDuplicateException("Email이 중복되었습니다.");
+		}
+		if (userService.isDuplicateNickname(signUpRequest.getNickname())) {
+			throw new EmailOrNicknameDuplicateException("Nickname이 중복되었습니다.");
+		}
 		User user = userService.signUp(signUpRequest);
 
 		URI location = ServletUriComponentsBuilder
@@ -57,19 +67,25 @@ public class AuthController {
 
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
-		Authentication authentication = authenticationManager.authenticate(
-			new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+		Authentication authentication;
+		try {
+			authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new InvalidPasswordException();
+		}
+
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 
-		UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+		UserPrincipal userPrincipal = (UserPrincipal)authentication.getPrincipal();
 		String accessToken = jwtUtil.generateJwtToken(userPrincipal);
 		String refreshToken = jwtUtil.generateRefreshToken(userPrincipal);
 
 		redisUtil.setDataExpire(userPrincipal.getUsername(), refreshToken, JwtUtil.REFRESH_EXPIRATION_SECONDS);
 
-		CookieUtil.addCookie(response,"Authorization",accessToken,(int)JwtUtil.TOKEN_EXPIRATION_SECONDS);
-		CookieUtil.addCookie(response,"refresh",refreshToken,(int)JwtUtil.REFRESH_EXPIRATION_SECONDS);
-		JwtResponse jwtResponse = new JwtResponse(accessToken, refreshToken, userPrincipal.getId(), userPrincipal.getEmail());
+		CookieUtil.addCookie(response, "Refresh", refreshToken, (int)JwtUtil.REFRESH_EXPIRATION_SECONDS);
+		JwtResponse jwtResponse = new JwtResponse(accessToken, refreshToken, userPrincipal.getId(),
+			userPrincipal.getEmail());
 
 		return new ResponseEntity<>(jwtResponse, HttpStatus.OK);
 	}
@@ -77,12 +93,12 @@ public class AuthController {
 	@PostMapping("/refresh")
 	public ResponseEntity<?> tokenRefresh(HttpServletRequest request, HttpServletResponse response) {
 		Cookie refreshCookie = getRefreshTokenAtCookie(request).orElseThrow(
-			TokenRefreshException::new);
+			() -> new TokenRefreshException("Refresh Token이 존재하지 않습니다."));
 
 		String refreshToken = refreshCookie.getValue();
 		String username = jwtUtil.getUserNameFromJwtToken(refreshToken);
 		if (redisUtil.getData(username).isEmpty()) {
-			throw new TokenRefreshException();
+			throw new TokenRefreshException("유효한 Refresh Token이 아닙니다.");
 		}
 		UserPrincipal userPrincipal = (UserPrincipal)userDetailsService.loadUserByUsername(username);
 
@@ -96,7 +112,7 @@ public class AuthController {
 		if (cookies == null)
 			return Optional.empty();
 		for (Cookie cookie : cookies) {
-			if (cookie.getName().equals("refresh"))
+			if (cookie.getName().equals("Refresh"))
 				return Optional.of(cookie);
 		}
 		return Optional.empty();
@@ -105,7 +121,7 @@ public class AuthController {
 	@PostMapping("/logout")
 	public ResponseEntity<?> logout(HttpServletRequest request) {
 		Cookie refreshCookie = getRefreshTokenAtCookie(request).orElseThrow(
-			TokenRefreshException::new);
+			() -> new TokenRefreshException("Refresh Token이 존재하지 않습니다."));
 
 		String refreshToken = refreshCookie.getValue();
 		String username = jwtUtil.getUserNameFromJwtToken(refreshToken);
