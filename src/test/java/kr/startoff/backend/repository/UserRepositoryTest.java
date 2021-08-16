b4058271972e76bb11a281c63893bdc5aa2d3d8e
SeoSeunghun <proto_seo@naver.com>
2021-08-16 14:21:48 +0900

    [test][#11] userRepository Test 작성

---
+++ src/test/java/kr/startoff/backend/repository/UserRepositoryTest.java
@@ -0,0 +1,72 @@
+package kr.startoff.backend.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import kr.startoff.backend.entity.AuthProvider;
+import kr.startoff.backend.entity.User;
+
+@DataJpaTest
+class UserRepositoryTest {
+	private final String EMAIL = "proto_seo@naver.com";
+	private final String NICKNAME = "proto_seo";
+	private final String PASSWORD = "password";
+	private final User USER = User.builder()
+		.email(EMAIL)
+		.nickname(NICKNAME)
+		.password(PASSWORD)
+		.provider(AuthProvider.local)
+		.build();
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@BeforeEach
+	private void saveUser() {
+		userRepository.save(USER);
+	}
+
+	@AfterEach
+	private void deleteUser() {
+		userRepository.delete(USER);
+	}
+
+	@Test
+	public void findByEmailTest() throws Exception {
+		//given
+		//when
+		Optional<User> foundUser = userRepository.findByEmail(EMAIL);
+		//then
+		assertNotNull(foundUser);
+		assertTrue(foundUser.isPresent());
+		assertEquals(EMAIL, foundUser.get().getEmail());
+		assertEquals(PASSWORD, foundUser.get().getPassword());
+		assertEquals(NICKNAME, foundUser.get().getNickname());
+		assertEquals(AuthProvider.local, foundUser.get().getProvider());
+	}
+
+	@Test
+	public void existsUserByEmailTest() throws Exception {
+		//given
+		//when
+		boolean result = userRepository.existsUserByEmail(EMAIL);
+		//then
+		assertTrue(result);
+	}
+
+	@Test
+	public void existsUserByNicknameTest() throws Exception {
+		//given
+		//when
+		boolean result = userRepository.existsUserByNickname(NICKNAME);
+		//then
+		assertTrue(result);
+	}
+}
