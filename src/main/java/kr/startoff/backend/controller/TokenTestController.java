a1a0ff26ffb3ac33410c7858a6e923eaf91def47
SeoSeunghun <proto_seo@naver.com>
2021-08-17 14:41:34 +0900

    [feat][#16] 권한관련, cookie관련 테스트 controller

---
+++ src/main/java/kr/startoff/backend/controller/TokenTestController.java
@@ -0,0 +1,39 @@
+package kr.startoff.backend.controller;
+
+import java.util.Optional;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.startoff.backend.util.CookieUtil;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TokenTestController {
+	@GetMapping("/token/user")
+	@PreAuthorize("hasAnyRole('USER')")
+	public ResponseEntity<?> getUser(){
+		return ResponseEntity.ok("user");
+	}
+
+	@GetMapping("/token/admin")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<?> getAdmin(){
+		return ResponseEntity.ok("admin");
+	}
+
+	@GetMapping("/cookie")
+	public ResponseEntity<?> getCookie(HttpServletRequest request){
+		Optional<Cookie> cookie = CookieUtil.getCookie(request,"react");
+		if(cookie.isEmpty()){
+			return ResponseEntity.badRequest().body("쿠키가 없어요.");
+		}
+		return ResponseEntity.ok(cookie.get().toString());
+	}
+}
