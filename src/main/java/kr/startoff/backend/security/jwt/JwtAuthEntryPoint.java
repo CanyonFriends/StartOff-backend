61eebee008a182de3bec8f5ce2d847559c421603
SeoSeunghun <proto_seo@naver.com>
2021-08-17 14:41:01 +0900

    [refactor][#16] 예외처리시 custom Error Message를 반환하도록 변경

--- src/main/java/kr/startoff/backend/security/jwt/JwtAuthEntryPoint.java
+++ src/main/java/kr/startoff/backend/security/jwt/JwtAuthEntryPoint.java
@@ -1,13 +1,20 @@
 package kr.startoff.backend.security.jwt;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.startoff.backend.exception.ErrorInfo;
 
 @Component
 public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
@@ -16,6 +23,16 @@ public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 	public void commence(HttpServletRequest request,
 		HttpServletResponse response,
 		AuthenticationException authException) throws IOException {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+		try (OutputStream os = response.getOutputStream()) {
+			ObjectMapper objectMapper = new ObjectMapper();
+			ErrorInfo errorInfo = new ErrorInfo("AccessToken 을 확인해주세요",
+				HttpStatus.UNAUTHORIZED.toString(), request.getRequestURI());
+			objectMapper.writeValue(os, errorInfo);
+			os.flush();
+		}
 	}
 }
