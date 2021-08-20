c2ca063b31a7da03275a124b5607ff01cf8f7140
SeoSeunghun <proto_seo@naver.com>
2021-08-20 13:38:14 +0900

    [feat][#8] Project Entity 생성
    - Project Entity 생성
    - 1:N 관계로 User Entity에 Mapping

--- src/main/java/kr/startoff/backend/entity/User.java
+++ src/main/java/kr/startoff/backend/entity/User.java
@@ -1,5 +1,9 @@
 package kr.startoff.backend.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -7,8 +11,11 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -60,6 +67,10 @@ public class User {
 
 	@Column(name = "image_url")
 	String imageUrl;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JsonIgnore
+	List<Project> projects = new ArrayList<>();
 
 	@Builder
 	public User(String email, String nickname, String password, AuthProvider provider) {
