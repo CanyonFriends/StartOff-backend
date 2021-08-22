41f84cb5e9c796308fe7ca160c0161052fcc4562
SeoSeunghun <proto_seo@naver.com>
2021-08-22 19:00:33 +0900

    [feat][#9] Skill Tag 관련 추가

---
+++ src/main/java/kr/startoff/backend/payload/response/SkillTagResponse.java
@@ -0,0 +1,21 @@
+package kr.startoff.backend.payload.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import kr.startoff.backend.entity.SkillTag;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SkillTagResponse {
+	private String skillName;
+	private String color;
+	private String textColor;
+
+	public SkillTagResponse(SkillTag skillTag) {
+		this.skillName = skillTag.getSkillName();
+		this.color = skillTag.getColor();
+		this.textColor = skillTag.getTextColor();
+	}
+}
--- src/main/java/kr/startoff/backend/payload/response/UserProfileResponse.java
+++ src/main/java/kr/startoff/backend/payload/response/UserProfileResponse.java
@@ -1,10 +1,16 @@
 package kr.startoff.backend.payload.response;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import kr.startoff.backend.entity.Project;
+import kr.startoff.backend.entity.SkillTag;
+import kr.startoff.backend.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,26 +19,21 @@ import lombok.Getter;
 public class UserProfileResponse {
 	@NotBlank
 	private String nickname;
-
 	private String introduce;
-
 	private String githubUrl;
-
 	private String blogUrl;
-
 	private String baekjoonId;
+	private List<ProjectResponse> projects;
+	private List<SkillTagResponse> userSkills;
 
-	// TODO : Projects, TechTags 추가 예정
-
-	@Builder
-	public UserProfileResponse(String nickname, String introduce, String githubUrl, String blogUrl,
-		String baekjoonId) {
-		this.nickname = nickname;
-		this.introduce = introduce;
-		this.githubUrl = githubUrl;
-		this.blogUrl = blogUrl;
-		this.baekjoonId = baekjoonId;
+	public UserProfileResponse(User user) {
+		this.nickname = user.getNickname();
+		this.introduce = user.getIntroduce();
+		this.githubUrl = user.getGithubUrl();
+		this.blogUrl = user.getBlogUrl();
+		this.baekjoonId = user.getBaekjoonId();
+		this.projects = user.getProjects().stream().map(ProjectResponse::new).collect(Collectors.toList());
+		this.userSkills = user.getUserSkills().stream().map(SkillTagResponse::new).collect(Collectors.toList());
 	}
-
 
 }
