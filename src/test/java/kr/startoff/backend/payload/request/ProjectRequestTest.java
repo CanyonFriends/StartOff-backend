0d1a70daed21b8e99d382c7d6ed3be1cac673b5c
SeoSeunghun <proto_seo@naver.com>
2021-08-24 22:00:16 +0900

    [test][#11] payload request body dto 테스트코드 작성

---
+++ src/test/java/kr/startoff/backend/payload/request/ProjectRequestTest.java
@@ -0,0 +1,39 @@
+package kr.startoff.backend.payload.request;
+
+import static kr.startoff.backend.payload.PayloadFixture.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+class ProjectRequestTest {
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	void projectAddRequestTest() throws Exception {
+		//given
+		String requestBody = "{\n"
+			+ "\"title\":\"Project Title\",\n"
+			+ "\"introduce\":\"Project Introduce\",\n"
+			+ "\"content\":\"Project Content\",\n"
+			+ "\"github_url\":\"https://github.com/Start-Off/StartOff-backend\",\n"
+			+ "\"deploy_url\":\"https://startoff.kr\",\n"
+			+ "\"start_date\":\"2021-07-26\",\n"
+			+ "\"end_date\":\"2021-09-30\",\n"
+			+ "\"project_skills\":[\"Spring Boot\",\"React\",\"Git\",\"AWS EC2\"]\n"
+			+ "}";
+		//when
+		ProjectRequest projectRequest = objectMapper.readValue(requestBody,
+			ProjectRequest.class);
+		//then
+		assertEquals(projectRequest.getTitle(), projectRequest().getTitle());
+		assertEquals(projectRequest.getIntroduce(), projectRequest().getIntroduce());
+		assertEquals(projectRequest.getContent(), projectRequest().getContent());
+		assertEquals(projectRequest.getGithubUrl(), projectRequest().getGithubUrl());
+		assertEquals(projectRequest.getDeployUrl(), projectRequest().getDeployUrl());
+		assertEquals(projectRequest.getStartDate(), projectRequest().getStartDate());
+		assertEquals(projectRequest.getEndDate(), projectRequest().getEndDate());
+		assertEquals(projectRequest.getProjectSkills(), projectRequest().getProjectSkills());
+	}
+}
---
+++ src/test/java/kr/startoff/backend/payload/request/RefreshOrLogoutRequestTest.java
@@ -0,0 +1,29 @@
+package kr.startoff.backend.payload.request;
+
+import static kr.startoff.backend.payload.PayloadFixture.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+class RefreshOrLogoutRequestTest {
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	void refreshOrLogoutRequestTest() throws Exception {
+		//given
+		String requestBody = "{\n"
+			+ "\"uuid\":\"uuid\",\n"
+			+ "\"email\":\"proto_seo@naver.com\",\n"
+			+ "\"access_token\":\"access token\"\n"
+			+ "}";
+		//when
+		RefreshOrLogoutRequest refreshOrLogoutRequest = objectMapper.readValue(requestBody,
+			RefreshOrLogoutRequest.class);
+		//then
+		assertEquals(refreshOrLogoutRequest.getUuid(), refreshOrLogoutRequest().getUuid());
+		assertEquals(refreshOrLogoutRequest.getEmail(), refreshOrLogoutRequest().getEmail());
+		assertEquals(refreshOrLogoutRequest.getAccessToken(), refreshOrLogoutRequest().getAccessToken());
+	}
+}
