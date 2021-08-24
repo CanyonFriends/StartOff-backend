b4b815364226d1ca1f4e0cea4c06ed9de92e01e7
SeoSeunghun <proto_seo@naver.com>
2021-08-24 22:00:16 +0900

    [test][#11] payload request body dto 테스트코드 작성

---
+++ src/test/java/kr/startoff/backend/payload/request/LoginRequestTest.java
@@ -0,0 +1,29 @@
+package kr.startoff.backend.payload.request;
+
+import static kr.startoff.backend.payload.PayloadFixture.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.startoff.backend.payload.request.profile.NicknameAndIntroduceRequest;
+
+class LoginRequestTest {
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+	@Test
+	void loginRequestTest() throws Exception {
+		//given
+		String requestBody = "{\n"
+			+ "\"email\":\"proto_seo@naver.com\",\n"
+			+ "\"password\":\"Password\"\n"
+			+ "}";
+		//when
+		LoginRequest loginRequest = objectMapper.readValue(requestBody,
+			LoginRequest.class);
+		//then
+		assertEquals(loginRequest.getEmail(), loginRequest().getEmail());
+		assertEquals(loginRequest.getPassword(), loginRequest().getPassword());
+	}
+}
