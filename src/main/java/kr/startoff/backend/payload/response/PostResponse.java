67da5f1c4b3a22650c554f298686fa5cabdffec8
SeoSeunghun <proto_seo@naver.com>
2021-09-03 17:00:15 +0900

    [feat][#26] Post관련 dto 구현

---
+++ src/main/java/kr/startoff/backend/payload/response/PostResponse.java
@@ -0,0 +1,39 @@
+package kr.startoff.backend.payload.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import kr.startoff.backend.entity.Category;
+import kr.startoff.backend.entity.Post;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostResponse {
+	Long postId;
+	String nickname;
+	String title;
+	String content;
+	Category category;
+	Integer maxPeople;
+	Integer currentPeople;
+	List<SkillTagResponse> postSkills;
+	String createdAt;
+	String updatedAt;
+
+	public PostResponse(Post post) {
+		this.postId = post.getId();
+		this.nickname = post.getAuthor().getNickname();
+		this.title = post.getTitle();
+		this.content = post.getContent();
+		this.category = post.getCategory();
+		this.maxPeople = post.getMaxPeople();
+		this.currentPeople = post.getCurrentPeople();
+		this.postSkills = post.getPostWantedSkills().stream().map(SkillTagResponse::new).collect(Collectors.toList());
+		this.createdAt = post.getCreatedAt().toString();
+		this.updatedAt = post.getUpdatedAt().toString();
+	}
+}
