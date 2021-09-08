654894b14b7fdd710971f4185f23646060dea27d
SeoSeunghun <proto_seo@naver.com>
2021-09-08 14:30:17 +0900

    [feat][#27] Comment 구현
    - Comment Domain 생성
    - 삭제 상태를 저장하기 위한 DeleteStatus Enum 생성

--- src/main/java/kr/startoff/backend/entity/Comment.java
+++ src/main/java/kr/startoff/backend/entity/Comment.java
@@ -2,19 +2,24 @@ package kr.startoff.backend.entity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import kr.startoff.backend.payload.request.CommentRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -31,23 +36,57 @@ public class Comment extends Auditor {
 	Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name = "parent_comment")
-	Comment parentComment;
+	@JoinColumn(name = "parent_id")
+	Comment parent;
 
-	@OneToMany(mappedBy = "parentComment",cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "parent", orphanRemoval = true)
 	@Column(name = "child_comments")
 	List<Comment> childComments = new ArrayList<>();
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name = "writer")
+	@JoinColumn(name = "user_id")
 	User writer;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name = "post")
+	@JoinColumn(name = "post_id")
 	Post post;
 
 	@Lob
 	@Column(name = "content")
 	String content;
+
+	@Column(name = "is_deleted")
+	@Enumerated(EnumType.STRING)
+	DeleteStatus isDeleted = DeleteStatus.N;
+
+	private void setParent(Comment parent) {
+		this.parent = parent;
+		parent.getChildComments().add(this);
+	}
+
+	public static Comment createComment(User writer, Post post, CommentRequest commentRequest) {
+		Comment comment = new Comment();
+
+		comment.setContent(commentRequest.getContent());
+		comment.setWriter(writer);
+		comment.setPost(post);
+
+		return comment;
+	}
+
+	public static Comment createComment(User writer, Post post, Comment parent, CommentRequest commentRequest) {
+		Comment comment = new Comment();
+
+		comment.setContent(commentRequest.getContent());
+		comment.setWriter(writer);
+		comment.setPost(post);
+		comment.setParent(parent);
+
+		return comment;
+	}
+
+	public void update(CommentRequest commentRequest) {
+		this.setContent(commentRequest.getContent());
+	}
 
 }
