f7edb15ab3feda4bbbecac9d9d04053585f90c01
SeoSeunghun <proto_seo@naver.com>
2021-09-08 14:37:48 +0900

    [feat][#27] posts 호출시 dto 따로 생성

---
+++ src/main/java/kr/startoff/backend/payload/response/PostListResponse.java
@@ -0,0 +1,33 @@
+package kr.startoff.backend.payload.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import kr.startoff.backend.entity.Post;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostListResponse {
+	Long postId;
+	String nickname;
+	String title;
+	Integer maxPeople;
+	Integer currentPeople;
+	List<SkillTagResponse> postSkills;
+	String createdAt;
+
+	public PostListResponse(Post post) {
+		this.postId = post.getId();
+		this.nickname = post.getAuthor().getNickname();
+		this.title = post.getTitle();
+		this.maxPeople = post.getMaxPeople();
+		this.currentPeople = post.getCurrentPeople();
+		this.createdAt = post.getCreatedAt().toString();
+		this.postSkills = post.getPostWantedSkills().stream().map(SkillTagResponse::new).collect(Collectors.toList());
+
+	}
+}
