81963d448ea114a715d46b8d95f2b5bfc7fa3cb3
SeoSeunghun <proto_seo@naver.com>
2021-11-03 17:44:05 +0900

    [feat][#38] WebSocket 관련 endpoint 및 관련 설정

---
+++ src/main/java/kr/startoff/backend/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package kr.startoff.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import kr.startoff.backend.handler.SignalHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(new SignalHandler(), "/meeting")
+			.setAllowedOrigins("*"); // allow all origins
+	}
+
+	@Bean
+	public ServletServerContainerFactoryBean createWebSocketContainer() {
+		ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+		container.setMaxTextMessageBufferSize(8192);
+		container.setMaxBinaryMessageBufferSize(8192);
+		return container;
+	}
+}
