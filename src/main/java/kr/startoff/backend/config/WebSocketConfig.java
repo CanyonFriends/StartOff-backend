38ca19d197ab97b45b96a3222997d5124162f4c9
SeoSeunghun <proto_seo@naver.com>
2021-11-17 15:12:50 +0900

    [refactor][#38] SignalHandler 생성자 주입으로 변경

--- src/main/java/kr/startoff/backend/config/WebSocketConfig.java
+++ src/main/java/kr/startoff/backend/config/WebSocketConfig.java
@@ -1,22 +1,25 @@
 package kr.startoff.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import kr.startoff.backend.handler.SignalHandler;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
+    private final SignalHandler signalHandler;
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new SignalHandler(), "/meeting")
+		registry.addHandler(signalHandler, "/meeting")
 			.setAllowedOrigins("*"); // allow all origins
 	}
 
