dce1f9cf1a7cf9aec0779753ac03ab7f88777df0
SeoSeunghun <proto_seo@naver.com>
2022-06-29 20:02:38 +0900

    [refactor][#46] user 관련 리팩터링
    - 이메일, 닉네임 중복 검사 API 로직 Service로 이동
    - 회원가입 로직 AuthService로 이동

--- src/main/java/kr/startoff/backend/service/UserService.java
+++ src/main/java/kr/startoff/backend/service/UserService.java
@@ -1,16 +1,15 @@
 package kr.startoff.backend.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import kr.startoff.backend.entity.AuthProvider;
 import kr.startoff.backend.entity.User;
 import kr.startoff.backend.exception.custom.EmailOrNicknameDuplicateException;
 import kr.startoff.backend.exception.custom.InvalidPasswordException;
 import kr.startoff.backend.exception.custom.UserNotFoundException;
-import kr.startoff.backend.payload.request.SignupRequest;
 import kr.startoff.backend.payload.request.UserPasswordChangeRequest;
 import kr.startoff.backend.payload.request.profile.BaekjoonIdRequest;
 import kr.startoff.backend.payload.request.profile.BlogUrlRequest;
@@ -26,46 +25,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserService {
 	private final UserRepository userRepository;
-	private final PasswordEncoder encoder;
 	private final S3UploadUtil s3UploadUtil;
-
-	@Transactional
-	public User signUp(SignupRequest request) {
-		User user = User.builder()
-			.email(request.getEmail())
-			.nickname(request.getNickname())
-			.password(encoder.encode(request.getPassword()))
-			.provider(AuthProvider.local)
-			.build();
-
-		return userRepository.save(user);
-	}
-
-	@Transactional(readOnly = true)
-	public boolean isDuplicateEmail(String email) {
-		return userRepository.existsUserByEmail(email);
-	}
-
-	@Transactional(readOnly = true)
-	public boolean isDuplicateNickname(String nickname) {
-		return userRepository.existsUserByNickname(nickname);
-	}
 
 	@Transactional(readOnly = true)
 	public UserInfoResponse getUserInformation(Long id) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		return new UserInfoResponse(user.getId(), user.getEmail(), user.getNickname());
 	}
 
 	@Transactional(readOnly = true)
 	public UserProfileResponse getUserProfile(Long id) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		return new UserProfileResponse(user);
 	}
 
 	@Transactional
 	public boolean changeUserPassword(UserPasswordChangeRequest updateRequest, Long id) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
 		if (!encoder.matches(updateRequest.getBeforePassword(), user.getPassword())) {
 			throw new InvalidPasswordException();
 		}
@@ -75,30 +52,27 @@ public class UserService {
 
 	@Transactional
 	public Long deleteUser(Long id) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		userRepository.delete(user);
 		return id;
 	}
 
 	@Transactional
 	public String updateNicknameAndIntroduce(Long id, NicknameAndIntroduceRequest nicknameRequest) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		String updateNickname = nicknameRequest.getNickname();
 		String updateIntroduce = nicknameRequest.getIntroduce();
-		if (!user.getNickname().equals(updateNickname)) {
-			if (userRepository.existsUserByNickname(updateNickname)) {
-				throw new EmailOrNicknameDuplicateException("Nickname이 중복되었습니다.");
-			}
-			user.setNickname(updateNickname);
+		if (!user.getNickname().equals(updateNickname) && isDuplicateNickname(updateNickname)) {
+			throw new EmailOrNicknameDuplicateException("Nickname이 중복되었습니다.");
 		}
+		user.setNickname(updateNickname);
 		user.setIntroduce(updateIntroduce);
-
 		return user.getNickname();
 	}
 
 	@Transactional
 	public String updateGithubUrl(Long id, GithubUrlRequest githubUrlRequest) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		String updateGithubUrl = githubUrlRequest.getGithubUrl();
 		user.setGithubUrl(updateGithubUrl);
 		return user.getGithubUrl();
@@ -106,7 +80,7 @@ public class UserService {
 
 	@Transactional
 	public String updateBlogUrl(Long id, BlogUrlRequest blogUrlRequest) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		String updateBlogUrl = blogUrlRequest.getBlogUrl();
 		user.setBlogUrl(updateBlogUrl);
 		return user.getBlogUrl();
@@ -114,7 +88,7 @@ public class UserService {
 
 	@Transactional
 	public String updateBaekjoonId(Long id, BaekjoonIdRequest baekjoonIdRequest) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		String updateBaekjoonId = baekjoonIdRequest.getBaekjoonId();
 		user.setBaekjoonId(updateBaekjoonId);
 		return user.getBaekjoonId();
@@ -122,13 +96,28 @@ public class UserService {
 
 	@Transactional
 	public String updateUserProfileImage(Long id, MultipartFile file) {
-		User user = getUserOrThrowException(id);
+		User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
 		String imageUrl = s3UploadUtil.uploadProfileImage(file, id);
 		user.setImageUrl(imageUrl);
 		return imageUrl;
 	}
 
-	private User getUserOrThrowException(Long id) {
-		return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+	@Transactional(readOnly = true)
+	public boolean validateEmailOrNickname(String email, String nickname) {
+		if (!email.equals("") && isDuplicateEmail(email)) {
+			throw new EmailOrNicknameDuplicateException("Email이 중복되었습니다.");
+		}
+		if (!nickname.equals("") && isDuplicateNickname(nickname)) {
+			throw new EmailOrNicknameDuplicateException("Nickname이 중복되었습니다.");
+		}
+		return email.equals("") != nickname.equals("");
+	}
+
+	private boolean isDuplicateEmail(String email) {
+		return userRepository.existsUserByEmail(email);
+	}
+
+	private boolean isDuplicateNickname(String nickname) {
+		return userRepository.existsUserByNickname(nickname);
 	}
 }
