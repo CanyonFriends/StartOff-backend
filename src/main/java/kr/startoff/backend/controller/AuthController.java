49477f467c7d244595584607e349a807abf47b7f
SeoSeunghun <proto_seo@naver.com>
2022-06-29 19:59:09 +0900

    [refactor][#46] Auth 관련 리팩토링
    - Controller 에서의 로직들 Service로 이동
    - Auth 관련 Service 생성

--- src/main/java/kr/startoff/backend/controller/AuthController.java
+++ src/main/java/kr/startoff/backend/controller/AuthController.java
@@ -1,39 +1,23 @@
 package kr.startoff.backend.controller;
 
 import java.net.URI;
-import java.util.Optional;
-import java.util.UUID;
 
 import javax.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import kr.startoff.backend.entity.User;
-import kr.startoff.backend.exception.custom.EmailOrNicknameDuplicateException;
-import kr.startoff.backend.exception.custom.InvalidPasswordException;
-import kr.startoff.backend.exception.custom.RefreshTokenException;
 import kr.startoff.backend.payload.request.LoginRequest;
 import kr.startoff.backend.payload.request.RefreshOrLogoutRequest;
 import kr.startoff.backend.payload.request.SignupRequest;
 import kr.startoff.backend.payload.response.AccessTokenResponse;
 import kr.startoff.backend.payload.response.CommonResponse;
 import kr.startoff.backend.payload.response.LoginResponse;
-import kr.startoff.backend.security.UserPrincipal;
-import kr.startoff.backend.security.jwt.JwtUtil;
-import kr.startoff.backend.service.UserDetailsServiceImpl;
-import kr.startoff.backend.service.UserService;
-import kr.startoff.backend.util.RedisUtil;
+import kr.startoff.backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,87 +26,34 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class AuthController {
-	private final AuthenticationManager authenticationManager;
-	private final UserService userService;
-	private final UserDetailsServiceImpl userDetailsService;
-	private final JwtUtil jwtUtil;
-	private final RedisUtil redisUtil;
+	private final AuthService authService;
 
 	@PostMapping("/signup")
-	public ResponseEntity<Long> signUp(@Valid @RequestBody SignupRequest signUpRequest) {
-		if (userService.isDuplicateEmail(signUpRequest.getEmail())) {
-			throw new EmailOrNicknameDuplicateException("Email이 중복되었습니다.");
-		}
-		if (userService.isDuplicateNickname(signUpRequest.getNickname())) {
-			throw new EmailOrNicknameDuplicateException("Nickname이 중복되었습니다.");
-		}
-		User user = userService.signUp(signUpRequest);
+	public ResponseEntity<Long> signup(@Valid @RequestBody SignupRequest signupRequest) {
+		Long userId = authService.signup(signupRequest);
 
 		URI location = ServletUriComponentsBuilder
 			.fromCurrentContextPath().path("/users/")
-			.buildAndExpand(user.getId()).toUri();
+			.buildAndExpand(userId).toUri();
 
-		return ResponseEntity.created(location).body(user.getId());
+		return ResponseEntity.created(location).body(userId);
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
-		Authentication authentication;
-		try {
-			authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
-		} catch (BadCredentialsException e) {
-			throw new InvalidPasswordException();
-		}
-
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-
-		UserPrincipal userPrincipal = (UserPrincipal)authentication.getPrincipal();
-		Long userId = userPrincipal.getId();
-		String email = userPrincipal.getEmail();
-		String nickname = userPrincipal.getNickname();
-		String accessToken = jwtUtil.generateJwtToken(userPrincipal);
-		String refreshToken = jwtUtil.generateRefreshToken(userPrincipal);
-		String uuid = UUID.randomUUID().toString();
-
-		redisUtil.setDataExpire(uuid, refreshToken, (int)JwtUtil.REFRESH_EXPIRATION_SECONDS);
-
-		LoginResponse jwtResponse = new LoginResponse(accessToken, uuid, userId, email, nickname);
-
-		return new ResponseEntity<>(jwtResponse, HttpStatus.OK);
+		LoginResponse loginResponse = authService.login(loginRequest);
+		return ResponseEntity.ok(loginResponse);
 	}
 
 	@PostMapping("/refresh")
-	public ResponseEntity<AccessTokenResponse> tokenRefresh(@RequestBody RefreshOrLogoutRequest request) {
-		String email = request.getEmail();
-		String uuid = request.getUuid();
-		String oldAccessToken = request.getAccessToken();
-		Optional<String> refreshToken = redisUtil.getData(uuid);
-
-		if (refreshToken.isEmpty()) {
-			throw new RefreshTokenException();
-		}
-		if (!jwtUtil.validateJwtToken(refreshToken.get()) ||
-			!email.equals(jwtUtil.getUserNameFromJwtToken(refreshToken.get()))) {
-			throw new RefreshTokenException();
-		}
-		UserPrincipal userPrincipal = (UserPrincipal)userDetailsService.loadUserByUsername(email);
-		String newAccessToken = jwtUtil.generateJwtToken(userPrincipal);
-
-		redisUtil.setDataExpire(oldAccessToken, "true", (int)JwtUtil.TOKEN_EXPIRATION_SECONDS);
-
-		return ResponseEntity.ok(new AccessTokenResponse(userPrincipal.getId(), newAccessToken));
+	public ResponseEntity<AccessTokenResponse> getAccessToken(@RequestBody RefreshOrLogoutRequest request) {
+		AccessTokenResponse accessTokenResponse = authService.refreshToken(request);
+		return ResponseEntity.ok(accessTokenResponse);
 	}
 
 	@PostMapping("/logout")
 	public ResponseEntity<CommonResponse> logout(@RequestBody RefreshOrLogoutRequest request) {
-		String uuid = request.getUuid();
-		String accessToken = request.getAccessToken();
-		if (redisUtil.getData(uuid).isPresent()) {
-			redisUtil.deleteData(uuid);
-		}
-		redisUtil.setDataExpire(accessToken, "true", (int)JwtUtil.TOKEN_EXPIRATION_SECONDS);
-		CommonResponse response = new CommonResponse(true, "로그아웃");
-		return ResponseEntity.ok(response);
+		CommonResponse commonResponse = authService.logout(request);
+		return ResponseEntity.ok(commonResponse);
 	}
 }
