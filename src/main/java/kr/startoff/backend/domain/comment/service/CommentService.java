25b8a465c80b26ca59513b294d7c976d7c443507
SeoSeunghun <proto_seo@naver.com>
2022-07-10 12:01:36 +0900

    [refactor][#48] comment 예외 관련 수정

--- src/main/java/kr/startoff/backend/domain/comment/service/CommentService.java
+++ src/main/java/kr/startoff/backend/domain/comment/service/CommentService.java
@@ -1,15 +1,17 @@
 package kr.startoff.backend.domain.comment.service;
 
+import static kr.startoff.backend.global.exception.ExceptionType.*;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.startoff.backend.domain.comment.domain.Comment;
 import kr.startoff.backend.domain.comment.domain.DeleteStatus;
+import kr.startoff.backend.domain.comment.exception.CommentException;
 import kr.startoff.backend.domain.post.domain.Post;
+import kr.startoff.backend.domain.post.exception.PostException;
 import kr.startoff.backend.domain.user.domain.User;
-import kr.startoff.backend.global.exception.custom.CommentNotFoundException;
-import kr.startoff.backend.global.exception.custom.PostNotFoundException;
-import kr.startoff.backend.global.exception.custom.UserNotFoundException;
+import kr.startoff.backend.domain.user.exception.UserException;
 import kr.startoff.backend.domain.comment.dto.CommentRequest;
 import kr.startoff.backend.domain.comment.dto.CommentResponse;
 import kr.startoff.backend.domain.comment.repository.CommentRepository;
@@ -26,11 +28,13 @@ public class CommentService {
 
 	@Transactional
 	public CommentResponse saveComment(Long postId, CommentRequest commentRequest) {
-		User writer = userRepository.findById(commentRequest.getUserId()).orElseThrow(UserNotFoundException::new);
-		Post post = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
+		User writer = userRepository.findById(commentRequest.getUserId())
+			.orElseThrow(() -> new UserException(USER_NOT_FOUND));
+		Post post = postRepository.findById(postId).orElseThrow(() -> new PostException(POST_NOT_FOUND));
 		Long parentId = commentRequest.getParentId();
 		if (hasParentComment(parentId)) {
-			Comment parent = commentRepository.findById(parentId).orElseThrow(CommentNotFoundException::new);
+			Comment parent = commentRepository.findById(parentId)
+				.orElseThrow(() -> new CommentException(COMMENT_NOT_FOUND));
 			return new CommentResponse(
 				commentRepository.save(Comment.createComment(writer, post, parent, commentRequest)));
 		} else {
@@ -40,14 +44,16 @@ public class CommentService {
 
 	@Transactional
 	public CommentResponse updateComment(Long commentId, CommentRequest commentRequest) {
-		Comment comment = commentRepository.findById(commentId).orElseThrow(CommentNotFoundException::new);
+		Comment comment = commentRepository.findById(commentId)
+			.orElseThrow(() -> new CommentException(COMMENT_NOT_FOUND));
 		comment.update(commentRequest);
 		return new CommentResponse(comment);
 	}
 
 	@Transactional
 	public Long deleteComment(Long commentId) {
-		Comment comment = commentRepository.findWithParentById(commentId).orElseThrow(CommentNotFoundException::new);
+		Comment comment = commentRepository.findWithParentById(commentId)
+			.orElseThrow(() -> new CommentException(COMMENT_NOT_FOUND));
 		if (comment.getChildComments().isEmpty()) {
 			commentRepository.delete(getDeletableAncestorComment(comment));
 		} else {
