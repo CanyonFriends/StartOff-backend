fb6bd509bb32968a7fc5d51085fd846ec505e29f
SeoSeunghun <proto_seo@naver.com>
2022-07-22 20:03:26 +0900

    [refactor][#53] DTO 관련 수정 및 Profile 기능 ProfileService로 이동

--- src/main/java/kr/startoff/backend/domain/user/service/UserService.java
+++ src/main/java/kr/startoff/backend/domain/user/service/UserService.java
@@ -7,14 +7,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.startoff.backend.domain.user.domain.User;
+import kr.startoff.backend.domain.user.dto.request.NicknameRequest;
 import kr.startoff.backend.domain.user.exception.UserException;
 import kr.startoff.backend.domain.user.dto.request.UserPasswordChangeRequest;
-import kr.startoff.backend.domain.user.dto.request.profile.BaekjoonIdRequest;
-import kr.startoff.backend.domain.user.dto.request.profile.BlogUrlRequest;
-import kr.startoff.backend.domain.user.dto.request.profile.GithubUrlRequest;
-import kr.startoff.backend.domain.user.dto.request.profile.NicknameAndIntroduceRequest;
 import kr.startoff.backend.domain.user.dto.response.UserInfoResponse;
-import kr.startoff.backend.domain.user.dto.response.UserProfileResponse;
 import kr.startoff.backend.domain.user.repository.UserRepository;
 import kr.startoff.backend.global.exception.ExceptionType;
 import kr.startoff.backend.global.util.S3UploadUtil;
@@ -30,12 +26,6 @@ public class UserService {
 	public UserInfoResponse getUserInformation(Long id) {
 		User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionType.USER_NOT_FOUND));
 		return new UserInfoResponse(user.getId(), user.getEmail(), user.getNickname());
-	}
-
-	@Transactional(readOnly = true)
-	public UserProfileResponse getUserProfile(Long id) {
-		User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionType.USER_NOT_FOUND));
-		return new UserProfileResponse(user);
 	}
 
 	@Transactional
@@ -57,40 +47,14 @@ public class UserService {
 	}
 
 	@Transactional
-	public String updateNicknameAndIntroduce(Long id, NicknameAndIntroduceRequest nicknameRequest) {
+	public String updateNickname(Long id, NicknameRequest nicknameRequest) {
 		User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionType.USER_NOT_FOUND));
 		String updateNickname = nicknameRequest.getNickname();
-		String updateIntroduce = nicknameRequest.getIntroduce();
-		if (!user.getNickname().equals(updateNickname) && isDuplicateNickname(updateNickname)) {
+		if (isDuplicateNickname(updateNickname)) {
 			throw new UserException(ExceptionType.DUPLICATE_NICKNAME);
 		}
 		user.updateNickname(updateNickname);
-		user.updateIntroduce(updateIntroduce);
 		return user.getNickname();
-	}
-
-	@Transactional
-	public String updateGithubUrl(Long id, GithubUrlRequest githubUrlRequest) {
-		User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionType.USER_NOT_FOUND));
-		String updateGithubUrl = githubUrlRequest.getGithubUrl();
-		user.updateGithubUrl(updateGithubUrl);
-		return user.getGithubUrl();
-	}
-
-	@Transactional
-	public String updateBlogUrl(Long id, BlogUrlRequest blogUrlRequest) {
-		User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionType.USER_NOT_FOUND));
-		String updateBlogUrl = blogUrlRequest.getBlogUrl();
-		user.updateBlogUrl(updateBlogUrl);
-		return user.getBlogUrl();
-	}
-
-	@Transactional
-	public String updateBaekjoonId(Long id, BaekjoonIdRequest baekjoonIdRequest) {
-		User user = userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionType.USER_NOT_FOUND));
-		String updateBaekjoonId = baekjoonIdRequest.getBaekjoonId();
-		user.updateBaekjoonId(updateBaekjoonId);
-		return user.getBaekjoonId();
 	}
 
 	@Transactional
