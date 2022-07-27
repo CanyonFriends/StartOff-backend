659d67737e193b24039f6d41daebeb41003d6aac
SeoSeunghun <proto_seo@naver.com>
2022-07-27 23:34:15 +0900

    [feat][#53] PasswordEncoder Bean으로 등록

--- src/main/java/kr/startoff/backend/global/config/SecurityConfig.java
+++ src/main/java/kr/startoff/backend/global/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import kr.startoff.backend.global.security.jwt.AuthTokenFilter;
@@ -55,9 +56,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new AuthTokenFilter(jwtUtil, redisUtil, userDetailsService);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
