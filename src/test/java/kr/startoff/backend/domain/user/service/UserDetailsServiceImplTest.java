6970f63c686b27823391a5be9d3731f366e954c3
SeoSeunghun <proto_seo@naver.com>
2022-07-27 23:35:26 +0900

    [test][#53] UserDetialsServiceImpl 관련 테스트 생성

--- src/test/java/kr/startoff/backend/domain/user/service/UserDetailsServiceImplTest.java
+++ src/test/java/kr/startoff/backend/domain/user/service/UserDetailsServiceImplTest.java
@@ -7,33 +7,41 @@ import static org.mockito.BDDMockito.*;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import kr.startoff.backend.domain.user.domain.User;
+import kr.startoff.backend.domain.user.exception.UserException;
 import kr.startoff.backend.domain.user.repository.UserRepository;
 import kr.startoff.backend.domain.user.domain.security.UserPrincipal;
 
+@ExtendWith(MockitoExtension.class)
 class UserDetailsServiceImplTest {
-	private UserRepository userRepository;
+	private final UserRepository userRepository = mock(UserRepository.class);
+
+	@InjectMocks
 	private UserDetailsServiceImpl userDetailsService;
-	@BeforeEach
-	private void setUp() {
-		userRepository = mock(UserRepository.class);
-		userDetailsService = new UserDetailsServiceImpl(userRepository);
-	}
 
 	@Test
-	void loadUserByUsernameTest() throws Exception {
+	void 이메일을_통해서_UserPrincipal_조회_성공() {
 		User user = user();
 		SimpleGrantedAuthority role = new SimpleGrantedAuthority("ROLE_USER");
 		given(userRepository.findByEmail(EMAIL)).willReturn(Optional.of(user));
 
-		UserPrincipal userPrincipal = (UserPrincipal) userDetailsService.loadUserByUsername(EMAIL);
+		UserPrincipal userPrincipal = (UserPrincipal)userDetailsService.loadUserByUsername(EMAIL);
 
 		assertEquals(user.getNickname(), userPrincipal.getNickname());
 		assertEquals(user.getEmail(), userPrincipal.getEmail());
 		assertTrue(userPrincipal.getAuthorities().contains(role));
+	}
+
+	@Test
+	void 이메일을_통해서_UserPrincipal_조회_실패() {
+		given(userRepository.findByEmail(EMAIL)).willThrow(UserException.class);
+
+		assertThrows(UserException.class, () -> userDetailsService.loadUserByUsername(EMAIL));
 	}
 }
