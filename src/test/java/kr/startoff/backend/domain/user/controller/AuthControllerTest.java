ba85e0bf1e586fda07abfa1836a5d223e78977d1
SeoSeunghun <proto_seo@naver.com>
2022-07-29 00:00:11 +0900

    [test][#53] AuthController 테스트 작성

--- src/test/java/kr/startoff/backend/domain/user/controller/AuthControllerTest.java
+++ src/test/java/kr/startoff/backend/domain/user/controller/AuthControllerTest.java
@@ -4,22 +4,19 @@ import static kr.startoff.backend.domain.user.fixture.UserFixture.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.List;
-import java.util.Optional;
+import static kr.startoff.backend.global.exception.ExceptionType.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -28,19 +25,14 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import kr.startoff.backend.domain.user.dto.request.LogoutRequest;
+import kr.startoff.backend.domain.user.dto.request.RefreshRequest;
 import kr.startoff.backend.domain.user.exception.UserException;
+import kr.startoff.backend.global.common.dto.CommonResponse;
 import kr.startoff.backend.global.config.SecurityConfig;
-import kr.startoff.backend.domain.user.controller.AuthController;
-import kr.startoff.backend.domain.user.domain.User;
 import kr.startoff.backend.domain.user.dto.request.LoginRequest;
 import kr.startoff.backend.domain.user.dto.request.SignupRequest;
-import kr.startoff.backend.global.exception.ExceptionType;
-import kr.startoff.backend.domain.user.domain.security.UserPrincipal;
-import kr.startoff.backend.global.security.jwt.JwtUtil;
 import kr.startoff.backend.domain.user.service.AuthService;
-import kr.startoff.backend.domain.user.service.UserDetailsServiceImpl;
-import kr.startoff.backend.domain.user.service.UserService;
-import kr.startoff.backend.global.util.RedisUtil;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = AuthController.class,
@@ -50,191 +42,159 @@ import kr.startoff.backend.global.util.RedisUtil;
 )
 class AuthControllerTest {
 	@MockBean
-	private AuthenticationManager authenticationManager;
-	@MockBean
-	private UserService userService;
-	@MockBean
 	private AuthService authService;
-	@MockBean
-	private UserDetailsServiceImpl userDetailsService;
-	@MockBean
-	private JwtUtil jwtUtil;
-	@MockBean
-	private RedisUtil redisUtil;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
 
 	private MockMvc mockMvc;
 	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	@BeforeEach
-	private void setUp(WebApplicationContext webApplicationContext) {
+	public void setup() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
-	@Test
-	void signUpSuccessTest() throws Exception {
-		SignupRequest request = signupRequest();
-		User user = user();
-		given(userService.validateEmailOrNickname(request.getEmail(), request.getNickname())).willReturn(true);
-		given(authService.signup(any())).willReturn(USER_ID);
+	@Nested
+	@DisplayName("회원가입 테스트")
+	class SignupTest {
+		private final SignupRequest signupRequest = signupRequest();
 
-		String content = objectMapper.writeValueAsString(request);
+		@Test
+		void 회원가입_성공() throws Exception {
+			given(authService.signup(any(SignupRequest.class))).willReturn(USER_ID);
+			String content = objectMapper.writeValueAsString(signupRequest);
 
-		ResultActions resultActions = mockMvc.perform(post("/api/v1/signup")
-			.content(content)
-			.contentType(MediaType.APPLICATION_JSON)
-			.accept(MediaType.APPLICATION_JSON));
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/signup")
+				.content(content)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
 
-		resultActions.andExpect(status().isCreated())
-			.andExpect(content().string(Long.toString(user.getId())));
+			resultActions.andExpect(status().isCreated())
+				.andExpect(content().string(Long.toString(USER_ID)));
+		}
+
+		@Test
+		void 중복된_이메일_회원가입_실패() throws Exception {
+			given(authService.signup(any(SignupRequest.class))).willThrow(new UserException(DUPLICATE_EMAIL));
+			String content = objectMapper.writeValueAsString(signupRequest);
+
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/signup")
+				.content(content)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+
+			resultActions.andExpect(status().isConflict())
+				.andExpect(jsonPath("message").value(DUPLICATE_EMAIL.getMessage()));
+		}
+
+		@Test
+		void 중복된_닉네임_회원가입_실패() throws Exception {
+			given(authService.signup(any(SignupRequest.class))).willThrow(new UserException(DUPLICATE_NICKNAME));
+			String content = objectMapper.writeValueAsString(signupRequest);
+
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/signup")
+				.content(content)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+
+			resultActions.andExpect(status().isConflict())
+				.andExpect(jsonPath("message").value(DUPLICATE_NICKNAME.getMessage()));
+		}
 	}
 
-	@Test
-	void signUpThrowConflictEmailExceptionTest() throws Exception {
-		SignupRequest request = signupRequest();
-		given(authService.signup(any())).willThrow(
-			new UserException(ExceptionType.DUPLICATE_EMAIL));
+	@Nested
+	@DisplayName("로그인 테스트")
+	class LoginTest {
+		private final LoginRequest loginRequest = loginRequest();
 
-		String content = objectMapper.writeValueAsString(request);
+		@Test
+		void 로그인_성공() throws Exception {
+			given(authService.login(any(LoginRequest.class))).willReturn(loginResponse());
 
-		ResultActions resultActions = mockMvc.perform(post("/api/v1/signup")
-			.content(content)
-			.contentType(MediaType.APPLICATION_JSON)
-			.accept(MediaType.APPLICATION_JSON));
-
-		resultActions.andExpect(status().isConflict())
-			.andExpect(jsonPath("error_msg").value("Email이 중복되었습니다."));
-	}
-
-	@Test
-	void signUpThrowConflictNicknameExceptionTest() throws Exception {
-		SignupRequest signupRequest = signupRequest();
-		given(authService.signup(any())).willThrow(
-			new UserException(ExceptionType.DUPLICATE_NICKNAME));
-
-		String content = objectMapper.writeValueAsString(signupRequest);
-
-		ResultActions resultActions = mockMvc.perform(post("/api/v1/signup")
-			.content(content)
-			.contentType(MediaType.APPLICATION_JSON)
-			.accept(MediaType.APPLICATION_JSON));
-
-		resultActions.andExpect(status().isConflict())
-			.andExpect(jsonPath("error_msg").value("Nickname이 중복되었습니다."));
-	}
-
-	@Test
-	void loginSuccessTest() throws Exception {
-		LoginRequest loginRequest = loginRequest();
-		UserPrincipal userPrincipal = UserPrincipal.create(user());
-		UsernamePasswordAuthenticationToken token =
-			new UsernamePasswordAuthenticationToken(userPrincipal, "password",
-				List.of(new SimpleGrantedAuthority("ROLE_USER")));
-		given(authenticationManager.authenticate(
-			new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())))
-			.willReturn(token);
-		given(jwtUtil.generateJwtToken(any(UserPrincipal.class))).willReturn(ACCESS_TOKEN);
-
-		mockMvc.perform(post("/api/v1/login")
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/login")
 				.content(objectMapper.writeValueAsString(loginRequest))
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("access_token").value(ACCESS_TOKEN))
-			.andExpect(jsonPath("user_id").value(USER_ID))
-			.andExpect(jsonPath("email").value(EMAIL))
-			.andExpect(jsonPath("nickname").value(NICKNAME))
-			.andReturn();
-	}
+				.accept(MediaType.APPLICATION_JSON));
 
-	@Test
-	void loginThrowExceptionTest() throws Exception {
-		LoginRequest loginRequest = loginRequest();
-		given(authenticationManager.authenticate(
-			new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())))
-			.willThrow(BadCredentialsException.class);
+			resultActions.andExpect(status().isOk())
+				.andExpect(jsonPath("accessToken").value(ACCESS_TOKEN))
+				.andExpect(jsonPath("userId").value(USER_ID))
+				.andExpect(jsonPath("email").value(EMAIL))
+				.andExpect(jsonPath("nickname").value(NICKNAME))
+				.andExpect(jsonPath("uuid").value(USER_UUID))
+				.andReturn();
+		}
 
-		mockMvc.perform(post("/api/v1/login")
+		@Test
+		void 비밀번호_로그인_실패() throws Exception {
+			given(authService.login(any(LoginRequest.class))).willThrow(new UserException(INVALID_PASSWORD));
+
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/login")
 				.content(objectMapper.writeValueAsString(loginRequest))
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isBadRequest())
-			.andExpect(jsonPath("error_msg").value("비밀번호를 확인해주세요."))
-			.andReturn();
+				.accept(MediaType.APPLICATION_JSON));
+
+			resultActions.andExpect(status().isUnauthorized())
+				.andExpect(jsonPath("message").value(INVALID_PASSWORD.getMessage()))
+				.andReturn();
+		}
 	}
 
-	@Test
-	void refreshSuccessTest() throws Exception {
-		UserPrincipal userPrincipal = userPrincipal();
-		given(userDetailsService.loadUserByUsername(EMAIL)).willReturn(userPrincipal);
-		given(jwtUtil.generateJwtToken(userPrincipal)).willReturn(ACCESS_TOKEN);
+	@Nested
+	@DisplayName("토큰 재발급 테스트")
+	class RefreshTokenTest {
+		private final RefreshRequest request = refreshRequest();
 
-		mockMvc.perform(post("/api/v1/refresh")
-				.content(objectMapper.writeValueAsString(refreshRequest()))
+		@Test
+		void 토큰_재발급_성공() throws Exception {
+			given(authService.refreshToken(any(RefreshRequest.class))).willReturn(accessTokenResponse());
+
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/refresh")
+				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("user_id").value(USER_ID))
-			.andExpect(jsonPath("access_token").value(ACCESS_TOKEN))
-			.andReturn();
-	}
+				.accept(MediaType.APPLICATION_JSON));
 
-	@Test
-	void refreshThrowExceptionTest1() throws Exception {
-		given(redisUtil.getData(USER_UUID)).willReturn(Optional.empty());
+			resultActions.andExpect(status().isOk())
+				.andExpect(jsonPath("userId").value(USER_ID))
+				.andExpect(jsonPath("accessToken").value(ACCESS_TOKEN))
+				.andReturn();
+		}
 
-		mockMvc.perform(post("/api/v1/refresh")
-				.content(objectMapper.writeValueAsString(refreshRequest()))
+		@Test
+		void 토큰_재발급_실패() throws Exception {
+			given(authService.refreshToken(any(RefreshRequest.class))).willThrow(
+				new UserException(REFRESH_TOKEN_EXPIRED));
+
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/refresh")
+				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isUnauthorized())
-			.andReturn();
+				.accept(MediaType.APPLICATION_JSON));
+
+			resultActions.andExpect(status().isUnauthorized())
+				.andExpect(jsonPath("message").value(REFRESH_TOKEN_EXPIRED.getMessage()))
+				.andReturn();
+		}
 	}
 
-	@Test
-	void refreshThrowExceptionTest2() throws Exception {
+	@Nested
+	@DisplayName("로그아웃 테스트")
+	class LogoutTest {
+		private final LogoutRequest request = logoutRequest();
 
-		mockMvc.perform(post("/api/v1/refresh")
-				.content(objectMapper.writeValueAsString(refreshRequest()))
+		@Test
+		void 로그아웃_성공() throws Exception {
+			given(authService.logout(any(LogoutRequest.class))).willReturn(new CommonResponse(true, "로그아웃"));
+
+			ResultActions resultActions = mockMvc.perform(post("/api/v1/logout")
+				.content(objectMapper.writeValueAsString(request))
 				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isUnauthorized())
-			.andReturn();
-	}
+				.accept(MediaType.APPLICATION_JSON));
 
-	@Test
-	void refreshThrowExceptionTest3() throws Exception {
-		mockMvc.perform(post("/api/v1/refresh")
-				.content(objectMapper.writeValueAsString(refreshRequest()))
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isUnauthorized())
-			.andReturn();
-	}
-
-	@Test
-	void logoutWithRedisHaveDataTest() throws Exception {
-
-		mockMvc.perform(post("/api/v1/logout")
-				.content(objectMapper.writeValueAsString(logoutRequest()))
-				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-				.accept(MediaType.APPLICATION_JSON_UTF8))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("success").value(true))
-			.andExpect(jsonPath("msg").value("로그아웃"))
-			.andReturn();
-	}
-
-	@Test
-	void logoutWithRedisHaveNotDataTest() throws Exception {
-		given(redisUtil.getData(USER_UUID)).willReturn(Optional.empty());
-
-		mockMvc.perform(post("/api/v1/logout")
-				.content(objectMapper.writeValueAsString(logoutRequest()))
-				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-				.accept(MediaType.APPLICATION_JSON_UTF8))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("success").value(true))
-			.andExpect(jsonPath("msg").value("로그아웃"))
-			.andReturn();
+			resultActions.andExpect(status().isOk())
+				.andExpect(jsonPath("success").value(true))
+				.andExpect(jsonPath("msg").value("로그아웃"))
+				.andReturn();
+		}
 	}
 }
