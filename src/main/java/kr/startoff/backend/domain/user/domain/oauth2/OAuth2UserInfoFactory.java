b226229f9320f707d570781a8154db9484b6b73f
SeoSeunghun <proto_seo@naver.com>
2022-08-19 23:03:55 +0900

    [refactor][#53] 예외처리 관련 수정

--- src/main/java/kr/startoff/backend/domain/user/domain/oauth2/OAuth2UserInfoFactory.java
+++ src/main/java/kr/startoff/backend/domain/user/domain/oauth2/OAuth2UserInfoFactory.java
@@ -6,14 +6,16 @@ import java.util.Map;
 
 import kr.startoff.backend.domain.user.domain.AuthProvider;
 import kr.startoff.backend.domain.user.exception.UserException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OAuth2UserInfoFactory {
 	public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
 		if (registrationId.equalsIgnoreCase(AuthProvider.GITHUB.toString())) {
 			return new GithubOAuth2UserInfo(attributes);
 		} else {
-			throw new UserException(OAUTH2_LOGIN_UNAUTHORIZED);
-			// throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+			throw new UserException(OAUTH2_NOT_SUPPORTED);
 		}
 	}
 }
